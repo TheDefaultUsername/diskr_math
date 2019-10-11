7fbff4db5f28f0f9469eda11f11e3cc91cbf9f53
Юрий  Новиков <expa@router.lab209.kaf>
2019-10-11 12:47:15 +0300

    max path

---
+++ fifth/max_path.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+int size;
+
+int source;
+int target;
+
+int getway(std::vector<int>&way,std::vector<std::vector<int> > matrix) {
+
+    std::vector<int>used(size,-1);
+    way.clear();
+    used.at(source)=source;
+    std::queue<int> possible_points;
+    possible_points.push(source);
+    bool flag = true;
+    while (flag) {
+        int now;
+        now=possible_points.front();
+        if (now==target) {flag==false; break;} 
+        possible_points.pop();
+        for (int i = 0; i<size; i++) {
+            if ((matrix[now][i]>0) && used[i]==-1) {
+                possible_points.push(i);
+                used.at(i)=now;
+                if (i==target) {flag = false; break;}
+            }
+        }
+        if (possible_points.back()==target) { flag=false; break;} 
+        if (possible_points.size()==0) { flag=false; break;}
+    }
+    if (used[target]==-1) {return 0;}
+    int weight;
+    {
+        int i = target;
+        weight=matrix[used.at(i)][i];
+        while (i!=source) {
+            if(matrix[used.at(i)][i]<weight && matrix[used.at(i)][i]>0) weight=matrix[used.at(i)][i];
+            way.push_back(i);
+            i=used.at(i);
+        }
+        way.push_back(source);
+    }
+    return weight;
+}
+
+int main() {
+    source = 0;
+    target = 1;
+    std::cin>>size;
+    std::vector<std::vector<int> > matrix (size, std::vector<int>(size));
+    for(int i =0; i< size; i++) {
+        for(int j =0; j <size;j++){
+            std::cin>>matrix[i][j];
+        }
+    }
+
+    int weight;
+    int max_weight = 0;
+    std::vector<int> way;
+    bool flag = true;
+    while (flag)
+    {   weight = getway(way, matrix);
+        if (weight==0) {flag = false; break;}
+        for (int i=0; i<way.size()-1; i++) {
+            int a,b;
+            a=way.at(i+1);
+            b=way.at(i);
+            if (matrix[a][b]<=weight) matrix[a][b]=0;
+            if (weight>max_weight) max_weight=weight;
+        }
+    }
+    std::cout<<max_weight<<std::endl;
+}
